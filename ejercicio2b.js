c49496b4fb5434eaa9e575e02cb7ef4a667a0824
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-11-02 16:27:33 +0100

    feat: Multiplicate arrays with formatted number with 3 digits

---
+++ ejercicio2b.js
@@ -0,0 +1,31 @@
+const array1 = [
+    [1, 3, 5],
+    [7, 9, 7],
+    [5, 3, 1]
+];
+  
+const array2 = [
+    [2, 4, 6],
+    [8, 10, 8],
+    [6, 4, 2]
+];
+  
+let result = [];
+  
+for (let i = 0; i < array1.length; i++) {
+    result.push(
+        array1[i].map(function (value, index) {
+          return parseInt(array2[index][i], 10) * parseInt(value, 10);
+        })
+    );
+}
+
+for (let i = 0; i < result.length; i++) {
+    result[i].map(function (value, index) {
+        number = "000"+value;
+        result[i][index] = number.substring(number.length - 3);
+    });
+}
+
+document.getElementById("result").innerHTML = result;
+console.log(result);
