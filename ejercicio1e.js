d06c81c8120dd4d09ff611c6208919d84f7dd30d
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-11-02 15:42:51 +0100

    feat: Create and Animate 1000 buttons

---
+++ ejercicio1e.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function createButton(numberOfButtons)
+{
+    for (let i = 1; i <= numberOfButtons; i++ ) {
+        const button = document.createElement("button");
+        button.innerText = i+' botón';
+        button.id = 'button'+i;
+        document.body.appendChild(button);
+    }    
+}
+
+function animatedButton(numberOfButtons)
+{
+    for (let i = 1; i <= numberOfButtons; i++ ) {
+        const textButton = document.getElementById('button'+i);
+
+        textButton.onmouseenter = () => {
+            textButton.innerHTML = i+' botóN';
+        }
+        
+        textButton.onmouseleave = () => {
+            textButton.innerHTML = i+' botón';
+        }
+    }
+}
+
+createButton(1000);
+animatedButton(1000);
